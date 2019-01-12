f37051ce092b5fdaf09a69688d9fbd10608be333
James Burke <james.walter.burke@gmail.com>
2019-01-12 17:04:38 -0500

    Remove unused code

--- themes/cpr/templates/class-page.php
+++ themes/cpr/templates/class-page.php
@@ -20,26 +20,4 @@ class Page extends \Alleypack\WP_Component\Component {
 	 * @var string
 	 */
 	public $name = 'page';
-
-	/**
-	 * Hook into post being set.
-	 */
-	public function post_has_set() {
-		$this->append_child( $this->get_body() );
-		return $this;
-	}
-
-	public function get_body() {
-		$body = new \Alleypack\WP_Component\Body();
-		print_r($post); die();
-		return $body;
-	}
-
-	public function get_page() {
-
-	}
-
-	public function get_landing_page() {
-
-	}
 }
