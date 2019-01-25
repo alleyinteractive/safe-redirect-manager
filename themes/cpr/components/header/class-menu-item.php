edddb545f4adbecc73ad543ab05f4387db6fceed
Owen Stowe <ostowe@gmail.com>
2019-01-25 16:09:06 -0500

    Implement new menu and menu item traits

--- themes/cpr/components/header/class-menu-item.php
+++ themes/cpr/components/header/class-menu-item.php
@@ -12,10 +12,32 @@ namespace CPR\Component\Header;
  */
 class Menu_Item extends \WP_Components\Component {
 
+	use \WP_Components\WP_Menu_item;
+
 	/**
 	 * Unique component slug.
 	 *
 	 * @var string
 	 */
 	public $name = 'header-menu-item';
+
+	/**
+	 * Define a default config.
+	 *
+	 * @return array Default config.
+	 */
+	public function default_config() {
+		return [
+			'id'          => '',
+			'label'       => '',
+			'url'         => '',
+		];
+	}
+
+	/**
+	 * Callback function for classes to override.
+	 */
+	public function menu_item_has_set() {
+		$this->set_config_from_menu_item();
+	}
 }
--- themes/cpr/components/header/class-menu.php
+++ themes/cpr/components/header/class-menu.php
@@ -12,10 +12,28 @@ namespace CPR\Component\Header;
  */
 class Menu extends \WP_Components\Component {
 
+	use \WP_Components\WP_Menu;
+
 	/**
 	 * Unique component slug.
 	 *
 	 * @var string
 	 */
 	public $name = 'header-menu';
+
+	/**
+	 * Default config is the sections.
+	 *
+	 * @return array
+	 */
+	public function default_config() {
+		return [
+			'type'            => 'menu',
+			'menu_item_class' => '\CPR\Component\Header\Menu_Item',
+		];
+	}
+
+	public function menu_has_set() {
+		$this->parse_wp_menu();
+	}
 }
