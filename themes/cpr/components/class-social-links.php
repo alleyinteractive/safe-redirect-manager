c9cbf1fd174fafb31a079250fea766de9bf3e5b4
cahdeemer <chrstina@alley.co>
2019-01-31 09:11:45 -0500

    added social-links-item and incorporated it into social links. incorporates updates from wp-components

--- themes/cpr/components/class-social-links.php
+++ themes/cpr/components/class-social-links.php
@@ -18,4 +18,35 @@ class Social_Links extends \WP_Components\Component {
 	 * @var string
 	 */
 	public $name = 'social-links';
+
+/**
+	 * Define default children.
+	 *
+	 * @return array Default children.
+	 */
+	public function default_children() {
+		return [
+			( new Social_Links_Item() )
+				->merge_config(
+					[
+						'type' => 'facebook',
+						'url' => '#'
+					]
+				),
+			( new Social_Links_Item() )
+			->merge_config(
+				[
+					'type' => 'twitter',
+					'url' => '#'
+				]
+			),
+			( new Social_Links_Item() )
+				->merge_config(
+					[
+						'type' => 'instagram',
+						'url' => '#'
+					]
+			)
+		];
+	}
 }
