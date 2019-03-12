c12ac2a12532f4bdcda76d8dd032284f2ebaa7bd
Renato Alves <renato@alley.co>
2019-03-11 22:51:54 -0300

    CPR-292: phpcs fix

--- themes/cpr/components/templates/class-search.php
+++ themes/cpr/components/templates/class-search.php
@@ -60,8 +60,8 @@ class Search extends \WP_Components\Component {
 										'title'        => 'grid',
 										'eyebrow'      => 'small',
 									]
-								),	
-					] ),
+								),
+                ] ),
 
 			/**
 			 * Pagination for results.
