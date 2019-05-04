7d38c4ebbdb2cef868a3c8dd6889fc56bbcb5a1d
James Burke <james.walter.burke@gmail.com>
2019-05-04 11:41:00 -0400

    Update migration structure

--- themes/cpr/migration/class-migration.php
+++ themes/cpr/migration/class-migration.php
@@ -12,66 +12,89 @@ namespace CPR\Migration;
  */
 class Migration {
 
+	use \Alleypack\Singleton;
+
+	/**
+	 * Cache the data directory.
+	 *
+	 * @var array
+	 */
+	public $data_mapping = [];
+
+	/**
+	 * Easily work on the migration locally.
+	 *
+	 * @var string
+	 */
+	public $migration_scope = 'partial';
+
 	/**
 	 * Constructor.
 	 */
-	public function __construct() {
+	public function setup() {
 
 		// Load some AlleyPack modules.
 		\Alleypack\load_module( 'attachments', '1.0' );
-		\Alleypack\load_module( 'sync-script', '1.1' );
+		\Alleypack\load_module( 'block-converter', '1.0' );
+		\Alleypack\load_module( 'sync-script', '1.2' );
 
-		// Story Migration.
-		require_once CPR_PATH . '/migration/story/class-feed.php';
-		require_once CPR_PATH . '/migration/story/class-feed-item.php';
-		\Alleypack\Sync_Script\register_feed( '\CPR\Migration\Story\Feed' );
+		$this->load_and_register_feeds();
 
-		// Person Migration.
-		require_once CPR_PATH . '/migration/person/class-feed.php';
-		require_once CPR_PATH . '/migration/person/class-feed-item.php';
-		\Alleypack\Sync_Script\register_feed( '\CPR\Migration\Person\Feed' );
+		if ( defined( 'WP_CLI' ) && WP_CLI ) {
+			// require_once CPR_Path . '/migration/cli/class-cli.php';
+		}
 	}
 
 	/**
-	 * Return an array of all the JSON files in a migration data directory.
+	 * Get the path to the data.
 	 *
-	 * @param string $slug Slug of data object.
-	 * @return array
+	 * @return string
 	 */
-	public static function get_directory_json( $slug ) {
-		$directory = get_template_directory() . "/migration/{$slug}/data/";
-		$files     = scandir( $directory );
+	public function get_data_directory( $slug ) {
 
-		if ( empty( $files ) ) {
-			return [];
+		// Determine scope.
+		if ( 'partial' === $this->migration_scope ) {
+			$file_path = CPR_PATH . '/migration/test-data/';
+		} else {
+			$file_path = CPR_PATH . '/migration/data/';
 		}
 
-		// Remove directories.
-		$files = array_values(
-			array_filter(
-				array_map(
-					function ( $file ) {
-						if ( false !== strpos( $file, '.json' ) ) {
-							return $file;
-						}
-					},
-					$files
-				)
-			)
-		);
+		$file_path .= "{$slug}/";
 
-		return $files;
+		if ( ! is_dir( $file_path ) ) {
+			wp_die( "No migration data found for {$slug}." );
+		}
+
+		return $file_path;
 	}
 
 	/**
-	 * Load a JSON file.
+	 * Helper to get a source data file by the directory offset.
 	 *
-	 * @param int    $id   ID of object.
-	 * @param string $slug Slug of object type.
+	 * @param int $offset File offset.
 	 * @return array
 	 */
-	public static function load_json_data( $id, $slug ) {
-		$file = get_template_directory() . "/migration/{$slug}/data/{$id}.json";
+	public function get_source_data_by_offset( string $slug, int $offset ) : array {
+
+		if ( empty( $this->data_mapping[ $slug ] ) ) {
+			$this->build_mapping( $slug );
+		}
+
+		$id = absint( $this->data_mapping[ $offset ] ?? 0 );
+
+		return $this->get_source_data_by_id( $slug, $id );
+	}
+
+	/**
+	 * Helper to get a source data file by the file id.
+	 *
+	 * @param int $id Legacy ID.
+	 * @return array
+	 */
+	public function get_source_data_by_id( string $slug, int $id ) : array {
+
+		// Validate file exists.
+		$file = $this->get_data_directory( $slug ) . "{$id}.json";
 		if ( ! file_exists( $file ) ) {
 			return [];
 		}
@@ -79,12 +102,63 @@ class Migration {
 		$json_string = file_get_contents( $file );
 		return json_decode( $json_string, true );
 	}
+
+	/**
+	 * Build the file position to legacy id mapping.
+	 */
+	public function build_mapping( $slug ) {
+
+		// Scan data directory for files.
+		$files = scandir( $this->get_data_directory( $slug ) );
+
+		if ( ! empty( $files ) ) {
+			// Remove directories.
+			$files = array_values(
+				array_filter(
+					array_map(
+						function ( $file ) {
+							if ( false !== strpos( $file, '.json' ) ) {
+								return $file;
+							}
+						},
+						$files
+					)
+				)
+			);
+
+			$this->data_mapping = $files;
+		}
+	}
+
+	/**
+	 * Load feed files and register.
+	 */
+	public function load_and_register_feeds() {
+
+		// Generic feed since all our data is structured the same way.
+		require_once CPR_PATH . '/migration/feeds/class-feed.php';
+
+		// Jobs.
+		require_once CPR_PATH . '/migration/feeds/job/class-feed.php';
+		require_once CPR_PATH . '/migration/feeds/job/class-feed-item.php';
+		\Alleypack\Sync_Script\register_feed( '\CPR\Migration\Job\Feed' );
+
+		// Story Migration.
+		// require_once CPR_PATH . '/migration/story/class-feed.php';
+		// require_once CPR_PATH . '/migration/story/class-feed-item.php';
+		// \Alleypack\Sync_Script\register_feed( '\CPR\Migration\Story\Feed' );
+
+		// Person Migration.
+		// require_once CPR_PATH . '/migration/person/class-feed.php';
+		// require_once CPR_PATH . '/migration/person/class-feed-item.php';
+		// \Alleypack\Sync_Script\register_feed( '\CPR\Migration\Person\Feed' );
+	}
 }
 
 // Initalize class.
 add_action(
 	'init',
 	function() {
-		new Migration();
+		Migration::instance();
 	}
 );
