4baa9f101b07ef8ad38af730bba387cdc3b6e54b
James Burke <james.walter.burke@gmail.com>
2019-05-15 00:30:20 -0400

    Podcast single

---
+++ themes/cpr/components/templates/class-podcast-and-show.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Podcast and Show Single Template Component.
+ *
+ * @package CPR
+ */
+
+namespace CPR\Components\Templates;
+
+/**
+ * Podcast and Show template.
+ */
+class Podcast_And_Show extends \WP_Components\Component {
+
+	use \WP_Components\WP_Post;
+
+	/**
+	 * Unique component slug.
+	 *
+	 * @var string
+	 */
+	public $name = 'podcast-and-show-template';
+
+	/**
+	 * Hook into post being set.
+	 *
+	 * @return self
+	 */
+	public function post_has_set() : self {
+		$body = new \WP_Components\Body();
+		$body->children = array_filter( $this->get_components() );
+		$this->append_child( $body );
+		return $this;
+	}
+
+	/**
+	 * Get an array of all components.
+	 *
+	 * @return array
+	 */
+	public function get_components() : array {
+		$data = get_post_meta( $this->get_post_id(), 'settings', true );
+		return [
+			/**
+			 * Header.
+			 */
+			( new \CPR\Components\Podcast_And_Show\Header() )->set_post( $this->post ),
+
+			/**
+			 * Highlighted episodes.
+			 */
+			( new \CPR\Components\Modules\Content_List() )
+				->set_config( 'image_size', 'grid_item' )
+				->set_config( 'heading', __( 'Highlighted Episodes', 'cpr' ) )
+				->parse_from_fm_data( $data['highlighted_content'] ?? [], 4 )
+				->set_theme( 'gridLarge' )
+				->set_child_themes(
+					[
+						'content-item' => 'grid',
+						'title'        => 'grid',
+						'eyebrow'      => 'small',
+					]
+				),
+		];
+	}
+}
--- themes/cpr/inc/routing.php
+++ themes/cpr/inc/routing.php
@@ -139,6 +139,20 @@ function build_components_endpoint(
 			break;
 
 		/**
+		 * Podcast/Show single.
+		 */
+		case $wp_query->is_tax( 'podcast' ):
+		case $wp_query->is_tax( 'show' ):
+
+			// This is a Term_Post_Link, so get the linked post (since all the
+			// meta is there).
+			$post_id = \Alleypack\Term_Post_Link::get_post_from_term( $wp_query->get_queried_object_id() );
+
+			$head->set_post( $post_id );
+			$template = ( new Components\Templates\Podcast_And_Show() )->set_post( $post_id );
+			break;
+
+		/**
 		 * Term archives.
 		 */
 		case $wp_query->is_tax():
