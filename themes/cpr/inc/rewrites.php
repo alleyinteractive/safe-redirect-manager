593b7b0cd01b48a267efdf0f79e86d24c1d29e80
James Burke <james.walter.burke@gmail.com>
2019-05-23 19:31:46 -0400

    Section rewrites

--- themes/cpr/inc/rewrites.php
+++ themes/cpr/inc/rewrites.php
@@ -11,9 +11,28 @@ namespace CPR;
  * Custom rewrites for podcasts.
  */
 function podcast_rewrites() {
+
+	// Podcasts.
 	add_rewrite_rule(
 		'^(news|classical|indie)/podcast/([^/]+)/?$',
 		'index.php?podcast=$matches[2]&podcast-episode-post=$matches[2]',
+		'top'
+	);
+
+	// All content archive.
+	add_rewrite_rule( '^/all/?$', 'index.php?post_type=post', 'top' );
+	add_rewrite_rule( '^/all/page/?([0-9]{1,})/?$', 'index.php?post_type=post&paged=$matches[1]', 'top' );
+
+	// Section archive.
+	add_rewrite_rule(
+		'^(news|classical|indie)/all/?$',
+		'index.php?taxonomy=section&term=$matches[1]&post_type=post',
+		'top'
+	);
+
+	add_rewrite_rule(
+		'^(news|classical|indie)/all/page/?([0-9]{1,})/?$',
+		'index.php?taxonomy=section&term=$matches[1]&post_type=post&paged=$matches[2]',
 		'top'
 	);
 }
