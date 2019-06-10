675297138a178e0f26cc816379adb068b5ae3a24
Renato Alves <renato@alley.co>
2019-06-10 13:49:37 -0300

    Making Travis happy

--- themes/cpr/single-newsletter-single.php
+++ themes/cpr/single-newsletter-single.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * The main newsletter template file.
+ * The main newsletter single template file.
  *
  * @package CPR
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <body>
-<?php echo get_post_meta( get_the_ID(), 'newsletter_html', true ); ?>
+<?php echo get_post_meta( get_the_ID(), 'newsletter_html', true ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 </body>
 </html>
