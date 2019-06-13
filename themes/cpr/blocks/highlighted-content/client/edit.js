4e9f56eda66994811cee1491bb8df63ea08b6777
Renato Alves <renato@alley.co>
2019-06-12 22:45:18 -0300

    Fixing loading of innerblocks

--- themes/cpr/blocks/highlighted-content/client/edit.js
+++ themes/cpr/blocks/highlighted-content/client/edit.js
@@ -1,73 +1,130 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import IconPicker from './icons/index';
 
-const { __ } = wp.i18n;
-const { Fragment } = wp.element;
 const {
-  InnerBlocks,
-  RichText,
-  InspectorControls,
-} = wp.editor;
-const {
-  PanelBody,
-  ToggleControl,
-} = wp.components;
+  i18n: {
+    __,
+  },
+  editor: {
+    InspectorControls,
+    InnerBlocks,
+    RichText,
+  },
+  components: {
+    PanelBody,
+    ToggleControl,
+  },
+  data: {
+    withSelect,
+  },
+  element: {
+    Fragment,
+  },
+} = wp;
 
-const HighlightedItemEdit = (props) => {
-  const {
-    attributes,
-    setAttributes,
-    innerblocksContent,
-  } = props;
-  const {
-    heading,
-    icon,
-    enableToggle,
-  } = attributes;
+class HighlightedItemEdit extends React.PureComponent {
+  /**
+   * Set PropTypes for this component.
+   * @type {object}
+   */
+  static propTypes = {
+    attributes: PropTypes.shape({
+      heading: PropTypes.string.isRequired,
+      enableToggle: PropTypes.bool.isRequired,
+      icon: PropTypes.string.isRequired,
+    }).isRequired,
+    setAttributes: PropTypes.func.isRequired,
+    innerblocksContent: PropTypes.string.isRequired,
+  };
 
-  setAttributes({ innerblocksContent });
+  /**
+   * Component updated.
+   */
+  componentDidUpdate(prevProps) {
+    const {
+      innerblocksContent,
+      setAttributes,
+    } = this.props;
 
-  return (
-    <Fragment>
-      <InspectorControls>
-        <PanelBody>
-          <ToggleControl
-            label={__('Toggle content', 'cpr')}
-            checked={!! enableToggle}
-            onChange={(value) => {
-              setAttributes({ enableToggle: value });
-            }}
-          />
-        </PanelBody>
-      </InspectorControls>
-      <IconPicker
-        label={__('Icon', 'cpr')}
-        value={icon}
-        onChange={(value) => {
-          setAttributes({ icon: value });
-        }}
-      />
-      <RichText
-        value={heading}
-        onChange={(value) => {
-          setAttributes({ heading: value });
-        }}
-      />
-      <InnerBlocks
-        allowedBlocks={['core/paragraph']}
-      />
-    </Fragment>
-  );
-};
+    if (0 !== prevProps.innerblocksContent.length) {
+      if (prevProps.innerblocksContent.length !== innerblocksContent.length) {
+        console.log(innerblocksContent);
+        setAttributes({ innerblocksContent });
+      }
+    }
+  }
 
-HighlightedItemEdit.propTypes = {
-  attributes: PropTypes.shape({
-    heading: PropTypes.string.isRequired,
-    enableToggle: PropTypes.bool.isRequired,
-    icon: PropTypes.string.isRequired,
-  }).isRequired,
-  setAttributes: PropTypes.func.isRequired,
-  innerblocksContent: PropTypes.string.isRequired,
-};
+  /**
+   * Renders this component.
+   * @returns {object} - JSX for this component.
+   */
+  render() {
+    const {
+      attributes: {
+        heading,
+        icon,
+        enableToggle,
+      },
+      setAttributes,
+    } = this.props;
 
-export default HighlightedItemEdit;
+    return (
+      <Fragment>
+        <InspectorControls>
+          <PanelBody>
+            <ToggleControl
+              label={__('Toggle content', 'cpr')}
+              checked={!! enableToggle}
+              onChange={(value) => {
+                setAttributes({ enableToggle: value });
+              }}
+            />
+          </PanelBody>
+        </InspectorControls>
+        <IconPicker
+          label={__('Icon', 'cpr')}
+          value={icon}
+          onChange={(value) => {
+            setAttributes({ icon: value });
+          }}
+        />
+        <RichText
+          value={heading}
+          onChange={(value) => {
+            setAttributes({ heading: value });
+          }}
+        />
+        <InnerBlocks
+          allowedBlocks={['core/paragraph']}
+        />
+      </Fragment>
+    );
+  }
+}
+
+const wrapWithSelect = withSelect((select, ownProps) => {
+  const { getBlock } = select('core/editor');
+  const { clientId } = ownProps;
+  const innerblocksContent = [];
+
+  if (null !== clientId) {
+    const parentBlock = getBlock(ownProps.clientId);
+    const innerBlocks = parentBlock.innerBlocks.length ?
+      parentBlock.innerBlocks :
+      [];
+
+    if (null !== innerBlocks.length) {
+      innerBlocks.forEach((block) => {
+        // Only those with content.
+        if (null !== block.attributes.content.length) {
+          innerblocksContent.push(block.attributes.content);
+        }
+      });
+    }
+  }
+
+  return { innerblocksContent };
+});
+
+export default wrapWithSelect(HighlightedItemEdit);
--- themes/cpr/blocks/highlighted-content/client/index.js
+++ themes/cpr/blocks/highlighted-content/client/index.js
@@ -1,10 +1,13 @@
 import HighlightedItemEdit from './edit';
 
-const { __ } = wp.i18n;
-const { registerBlockType } = wp.blocks;
 const {
-  withSelect,
-} = wp.data;
+  i18n: {
+    __,
+  },
+  blocks: {
+    registerBlockType,
+  },
+} = wp;
 
 registerBlockType('cpr/highlighted-content', {
   title: __('Highlighted Content', 'cpr'),
@@ -27,33 +30,7 @@ registerBlockType('cpr/highlighted-content', {
       default: 'fab fa-wordpress-simple',
     },
   },
-  edit: withSelect((select, ownProps) => {
-    const {
-      clientId,
-    } = ownProps;
-    const { getBlock } = select('core/editor');
-    const innerblocksContent = [];
-
-    if (null !== clientId) {
-      const parentBlock = getBlock(ownProps.clientId);
-      const innerBlocks = parentBlock.innerBlocks.length ?
-        parentBlock.innerBlocks :
-        [];
-
-      if (null !== innerBlocks.length) {
-        innerBlocks.forEach((block) => {
-          // Only those with content.
-          if (null !== block.attributes.content.length) {
-            innerblocksContent.push(block.attributes.content);
-          }
-        });
-      }
-    }
-
-    return {
-      innerblocksContent,
-    };
-  })(HighlightedItemEdit),
+  edit: HighlightedItemEdit,
   save: () => {
     return null;
   },
