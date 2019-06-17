e4ff5a503d6e7efc7085a3e90da8e251b50ffb76
jomurgel <jo@alley.co>
2019-06-17 08:32:19 -0600

    adds function to handle click when/if news/music buttons are clicked

--- themes/cpr/client/src/admin/admin.js
+++ themes/cpr/client/src/admin/admin.js
@@ -1,6 +1,7 @@
 import './blockEditor';
 import './editor';
 import { loadCustomImageDetailsTemplate } from './loadCustomImageDetailsTemplate';
+import { runAudioConversion } from './runAudioConversion';
 
 if (module.hot) {
   module.hot.accept();
@@ -8,4 +9,5 @@ if (module.hot) {
 
 document.addEventListener('DOMContentLoaded', () => {
   loadCustomImageDetailsTemplate();
+  runAudioConversion();
 });
---
+++ themes/cpr/client/src/admin/runAudioConversion.js
@@ -0,0 +1,13 @@
+/**
+ * Handle media encode in attachement view.
+ */
+/* eslint-disable */
+export function runAudioConversion() {
+  jQuery(document).ready(function( $ ) {
+    $( 'body' ).on( 'click', 'button.cpr-encode-audio', function( e ) {
+      // Make request to custom REST endpoint.
+      // Update contents of <span class="value"> if Processing/Encoded
+    });
+  });
+}
+/* eslint-enable */
