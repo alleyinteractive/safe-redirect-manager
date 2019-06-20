b692ad154e20c6708845ce8cb11f8c9dd13e309b
Zack Rothauser <zack@alley.co>
2019-06-20 10:32:59 -0700

    Remove debugging lines

--- themes/cpr/inc/routing.php
+++ themes/cpr/inc/routing.php
@@ -70,12 +70,6 @@ function build_components_endpoint(
 	// Begin building a new header instance for the request.
 	$header = ( new Components\Header\Header() )->set_cpr_header();
 
-	error_log( print_r( $wp_query->query, true ) );
-
-	error_log( print_r( 'tribe_events' === $wp_query->query->post_type, true ) );
-	error_log( print_r( isset( $wp_query->query->eventDisplay ), true ) );
-	error_log( print_r( 'month' === $wp_query->query->eventDisplay, true ) );
-
 	// Build page.
 	switch ( true ) {
 
